dd175cec77549080281283ce430ab8828ae5ae68
Maciej Marzęta <maciej@evidentid.com>
2020-01-25 13:52:04 +0100

    user euthorization and register

---
+++ auth/models.py
@@ -0,0 +1,37 @@
+from pydantic import BaseModel
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str = None
+
+
+class User(BaseModel):
+    username: str
+    email: str
+    full_name: str
+    pesel: str
+    disabled: bool = False
+
+
+class UserRegister(User):
+    plain_password: str
+
+
+class UserInDB(User):
+    hashed_password: str
+
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
+        "disabled": False,
+    }
+}
